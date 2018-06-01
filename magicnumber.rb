e84b4a83f5e51b3c09dba7390397aa2c248f4859
Lee Klitzner <lee.klitzner@gmail.com>
2018-06-01 10:15:08 -0600

    functionality and text

--- magicnumber.rb
+++ magicnumber.rb
@@ -1,7 +1,32 @@
-require 'numbers_in_words'
+
+require 'humanize'
 
 def menu
-  puts 'Enter a Number: '
-  choice = gets.strip.to_i
-  magic(choice)
+  puts "Want to see me blow your mind?"
+  puts "1) Enter"
+  puts "2) Exit"
+  choice = gets.strip
+
+  case choice
+    when "1"
+        begin_game
+    when "2"
+        exit_game
+    else
+        puts "Make Another Selection"
+        menu
+  end
+end
+
+def begin_game
+    puts "Enter Any Number of Your Choosing"
+    
+    @num = gets.strip.to_i
+
+    puts @num.humanize
+end
+
+def exit_game
+    puts "See You Next Time!"
+    exit
 end
